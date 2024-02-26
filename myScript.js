14856974127a4856b9c97de6d1f2bfefce89a25b
Muhammad Tayyab <786.mtayyab@gmail.com>
2024-02-26 22:58:19 +0500

    Completed

---
+++ myScript.js
@@ -0,0 +1,155 @@
+                 //==========// CHAPTER # 1 Dated: 24th FEB 2024//==========//
+
+
+
+//Q # 1
+
+            //alert("Welcom to Our Home Page");
+
+//Q # 2
+            //alert("Error! Please Enter a valid Password")
+
+//Q # 3
+            //alert("Welcom to Js Land \n Happy Coding")            
+            
+//Q # 4 
+            //alert("Welcom to Js Land");
+            //alert("Happy Coding \n Prevent this page to additional dailogs");
+
+
+//Q # 5
+            //console.log("Hello! I can Run Js in my Web Browser's Console")
+
+//Q # 6
+            //alert("Use of Script in HTML / CSS project")
+            // check Head and Body 
+
+//Q # 7
+            //Plz check index File , i have use script on many places 
+
+
+
+
+
+                //==========// CHAPTER # 2 Dated: 24th FEB 2024//==========//
+
+
+
+//Q # 1
+
+
+            // var userName = "smit";
+            // alert(userName)
+            // document.write(userName)
+
+//Q # 2
+
+
+            // var myName = "Muhammad Tayyab";
+            // alert(myName)
+            // document.write(myName)
+            
+//Q # 3
+
+
+            // var message = "Hello World";
+            // alert(message)
+
+
+
+//Q # 4
+
+
+            // var myName = "Muhammad Tayyab";
+            // alert(myName)
+          
+            
+            // var myAge = "40 Years";
+            // alert(myAge)
+           
+
+            // var my_course = "Web & Mobile App Development";
+            // alert(my_course)
+            
+
+            // var my_inst = "SMIT";
+            // alert(my_inst)
+
+            // var campus = "Saylani Head Office";
+            // alert(campus)
+
+//Q # 5
+
+
+            // var X = "PIZZA \nPIZZ \nPIZ \nPI \nP";
+            // alert(X)
+
+//Q # 6
+
+
+            // var email = " 'tayyab123@google.com' ";
+            // alert("My Email Address is " + email)
+
+
+
+//Q # 7
+
+            // var book = " 'A smarter way to learn JavaScript' ";
+            // alert("I am trying to learn from the book " + book);
+
+//Q # 8
+            
+            // var A = " <h1>Yes i can Write JavaScript in HTML</h1><br>"
+            // document.write(A)    
+            
+            // var myName = " <h1>Muhammad Tayyab</h1><br>"
+            // document.write(myName)
+
+            // var myRollNo = "<h2>WMA-188638</h2><br>"
+            // document.write(myRollNo)
+
+
+//Q # 9            
+
+       
+
+            // alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+            // document.write("<h1>“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”</h1>");
+
+
+
+
+
+            //==========// CHAPTER # 3 Dated: 24th FEB 2024//==========//
+
+
+
+
+//Q # 1
+
+            // var age = "40 Years";
+            // alert("I am "+ age + " old.")
+
+//Q # 2     
+            // var vistr_no = "14";
+            // alert("You have visted this site" + vistr_no + " times")
+            // document.write("You have visted this site " + vistr_no + " times")
+
+            
+//Q # 3
+
+            // var birthYear = "1983";
+            // document.write("<h1>My Birth year is  "+ birthYear + "</h1>" + "<br> <h2>Data Type of My variable is <b><i><u>'Number'</h2>")             
+
+//Q # 4
+
+            // var v_name = "John Doe";
+            // var p_title = "T-Shirt's";
+            // var q_tity = 5;
+
+            // document.write("<b>"+ v_name + "</b>" + " ordered " + "<b>" +  q_tity + " " + p_title + "</b>" +" on XYZ Clothing Store"); 
+
+
+            
+
+
